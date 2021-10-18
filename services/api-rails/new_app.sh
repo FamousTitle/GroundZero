2e5d5035572dac6c32e276e74d12bde986d9b3af
Tha Leang <vleango@gmail.com>
2021-10-18 15:02:50 -0700

    update famoustitle_rails to 082c080

--- services/api-rails/new_app.sh
+++ services/api-rails/new_app.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 if [ -n "$1" ]; then
-  rails new $1 -d=mysql --skip-bundle --skip-git --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-action-cable --skip-turbolinks --skip-test --skip-system-test --skip-javascript
+  rails new $1 -d=mysql --skip-bundle --skip-git --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-action-cable --skip-turbolinks --skip-test --skip-system-test --skip-javascript --force
   mv -n $1/* .
   rm -rf $1
 
-  echo "gem 'famoustitle_rails', git: 'https://github.com/vleango/famoustitle-rails', ref: 'f58a304'" >> /app/Gemfile
+  echo "gem 'famoustitle_rails', git: 'https://github.com/vleango/famoustitle-rails', ref: '082c080'" >> /app/Gemfile
   bundle
 
+  echo "hi"
   rails g famoustitle_rails:install
+  echo "bye"
 
   echo "$1 created!"
 else
