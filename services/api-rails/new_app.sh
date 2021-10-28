d26d5d18327f481ce48e0e3df19eec86de94d1bd
Tha Leang <vleango@gmail.com>
2021-10-27 21:16:04 -0700

    use famoustitle-rails 1.2.0 - goldiloader fix

--- services/api-rails/new_app.sh
+++ services/api-rails/new_app.sh
@@ -5,7 +5,7 @@ if [ -n "$1" ]; then
   mv -n $1/* .
   rm -rf $1
 
-  echo "gem 'famoustitle_rails', git: 'https://github.com/vleango/famoustitle-rails', tag: '1.1.0'" >> /app/Gemfile
+  echo "gem 'famoustitle_rails', git: 'https://github.com/vleango/famoustitle-rails', tag: '1.2.0'" >> /app/Gemfile
   bundle
 
   rails g famoustitle_rails:install
