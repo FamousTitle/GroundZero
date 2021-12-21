8c1cf5d6de34aeb0f26ec588c9e2537fbb95c16a
Tha Leang <vleango@gmail.com>
2021-12-21 01:19:14 -0800

    update famoustitle-rails to use 1.3.0 (rails 7 from fameauth support)

--- services/api-rails/new_app.sh
+++ services/api-rails/new_app.sh
@@ -5,7 +5,7 @@ if [ -n "$1" ]; then
   mv -n $1/* .
   rm -rf $1
 
-  echo "gem 'famoustitle_rails', git: 'https://github.com/vleango/famoustitle-rails', tag: '1.2.1'" >> /app/Gemfile
+  echo "gem 'famoustitle_rails', git: 'https://github.com/vleango/famoustitle-rails', tag: '1.3.0'" >> /app/Gemfile
   bundle
 
   rails g famoustitle_rails:install
