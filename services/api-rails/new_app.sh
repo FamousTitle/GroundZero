f8c4f72b036e291c8c28d8b6d21f2615943f12cf
Tha Leang <vleango@gmail.com>
2022-05-06 21:52:06 -0500

    fix build error when a bad install occurred

--- services/api-rails/new_app.sh
+++ services/api-rails/new_app.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ -n "$1" ]; then
+
+  rm -rf ./config log storage tmp
+
   rails new $1 -d=postgresql --skip-bundle --skip-git --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-action-cable --skip-asset-pipeline --skip-javascript --skip-hotwire --skip-jbuilder --skip-test --skip-system-test --force
   mv -n $1/* .
   rm -rf $1
