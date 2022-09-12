2ef8385c9daf790a20779ee3a0cb3100a52dcae4
Tha Leang <vleango@gmail.com>
2022-09-12 14:09:55 -0500

    update image for sudo, famoustitle-rails for latest defaults

--- new_app.sh
+++ new_app.sh
@@ -2,11 +2,7 @@
 
 if [ -n "$1" ]; then
 
-  rm -rf ./services/web-rails/config log storage tmp
-
-docker-compose run --rm web-rails rails new $1 -d=postgresql --skip-bundle --skip-git --skip-test --skip-system-test --force
-  mv -n $1/* .
-  rm -rf $1
+  docker-compose run --rm web-rails ./new_app.sh $1
 
   echo "all done!"
 else
--- services/web-rails/new_app.sh
+++ services/web-rails/new_app.sh
@@ -4,9 +4,14 @@ if [ -n "$1" ]; then
 
   rm -rf ./config log storage tmp
 
-  rails new $1 -d=postgresql --skip-bundle --skip-git --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-action-cable --skip-asset-pipeline --skip-javascript --skip-hotwire --skip-jbuilder --skip-test --skip-system-test --force
+  rails new $1 -d=postgresql --skip-bundle --skip-git --skip-test --skip-system-test --force
   mv -n $1/* .
   rm -rf $1
+
+  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: '048a32db725a814829885835f9c61d545f40de70'" >> /app/Gemfile
+  bundle
+
+  rails g famoustitle_rails:install
 
   echo "$1 api-rails created!"
 else
