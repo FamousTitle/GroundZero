8bdd43f8e116ccf80d839e88771619009306cfd7
Tha Leang <vleango@gmail.com>
2022-09-13 11:23:51 -0700

    use v2.0.0 of famoustitle-rails and update start command for rails 7

--- services/web-rails/new_app.sh
+++ services/web-rails/new_app.sh
@@ -8,7 +8,7 @@ if [ -n "$1" ]; then
   mv -n $1/* .
   rm -rf $1
 
-  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: '048a32db725a814829885835f9c61d545f40de70'" >> /app/Gemfile
+  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: 'c88c6ebbabd86ea2baa67510c7c7167f886cde6f'" >> /app/Gemfile
   bundle
 
   rails g famoustitle_rails:install
