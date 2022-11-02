60c3635da6c09093059bf095e7e95f5d6c6f4fc8
Tha Leang <vleango@gmail.com>
2022-11-02 00:03:11 -0500

    use famoustitle-rails to fix bin/dev foreman install issue

--- services/web-rails/new_app.sh
+++ services/web-rails/new_app.sh
@@ -8,7 +8,7 @@ if [ -n "$1" ]; then
   mv -n $1/* .
   rm -rf $1
 
-  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: 'c88c6ebbabd86ea2baa67510c7c7167f886cde6f'" >> /app/Gemfile
+  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: '31e991892f83ca1c1ce3920da81ecdf8d39f8603'" >> /app/Gemfile
   bundle
 
   rails g famoustitle_rails:install
