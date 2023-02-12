a6eb8b2add5e5a85d7ddcf43e694d9960fe4221c
Tha Leang <vleango@gmail.com>
2023-02-11 22:09:04 -0600

    use latest famoustitle-rails

--- services/web-rails/new_app.sh
+++ services/web-rails/new_app.sh
@@ -8,7 +8,7 @@ if [ -n "$1" ]; then
   mv -n $1/* .
   rm -rf $1
 
-  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: '5bda43bc7bcabeab1680c67b64af949b1f603361'" >> /app/Gemfile
+  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: '90e29d3e899e63d73541a83b2adf67d16d2db0a6'" >> /app/Gemfile
   bundle
 
   rails g famoustitle_rails:install
