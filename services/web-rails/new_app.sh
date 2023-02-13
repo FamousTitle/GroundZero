72670df90310c5868a85aa40981e4649b773400f
Tha Leang <vleango@gmail.com>
2023-02-13 00:48:49 -0600

    add graphiql and updated Famoustitle-rails

--- services/web-rails/new_app.sh
+++ services/web-rails/new_app.sh
@@ -8,7 +8,7 @@ if [ -n "$1" ]; then
   mv -n $1/* .
   rm -rf $1
 
-  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: '968adcb2bc102db70e09a0ef2fb5a2e45cc0ea2f'" >> /app/Gemfile
+  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: 'fc28578d180c2bf1ae104c1ce03e67dfac948e53'" >> /app/Gemfile
   bundle
 
   rails g famoustitle_rails:install
