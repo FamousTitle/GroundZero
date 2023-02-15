94dca0517497de4f8d33cd22d6740ef9fab9f39a
Tha Leang <vleango@gmail.com>
2023-02-15 13:54:37 -0600

    update famoustitle-rails to 14a27730d9b1f8c8b9b7dda841171bbf928f28b3

--- services/web-rails/new_app.sh
+++ services/web-rails/new_app.sh
@@ -8,7 +8,7 @@ if [ -n "$1" ]; then
   mv -n $1/* .
   rm -rf $1
 
-  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: 'f563c1a5771dfc46596ca9ab64a8e589b5ad9b00'" >> /app/Gemfile
+  echo "gem 'famoustitle_rails', git: 'https://github.com/FamousTitle/famoustitle-rails', ref: '14a27730d9b1f8c8b9b7dda841171bbf928f28b3'" >> /app/Gemfile
   bundle
 
   rails g famoustitle_rails:install
