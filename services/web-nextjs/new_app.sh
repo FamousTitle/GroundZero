af751418b5406f3f2555f5e4d44b630b5154d056
Tha Leang <vleango@gmail.com>
2024-01-23 01:38:18 -0600

    migrate the db automatically after create

--- services/web-nextjs/new_app.sh
+++ services/web-nextjs/new_app.sh
@@ -5,6 +5,10 @@ if [ -n "$1" ]; then
   mv -n $1/* .
   rm -rf $1
   echo "$1 web-nextjs created!"
+
+  npx prisma migrate dev
+  echo "database created!"
+  
 else
   echo "Need to set a project name!"
 fi
