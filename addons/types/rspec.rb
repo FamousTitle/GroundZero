1ae3531175d7906f981989bf3de8245435da4270
Tha Leang <vleango@gmail.com>
2025-11-21 15:47:31 -0600

    fix rspec addon, needed to move the require after rails loads

--- addons/types/rspec.rb
+++ addons/types/rspec.rb
@@ -10,9 +10,31 @@ class RspecAddon < Base
     # capybara
     run "bundle add capybara --group development,test"
     
-    # Add Capybara configuration to spec files
-    run 'echo "require \'capybara/rails\'" | cat - spec/rails_helper.rb > temp && mv temp spec/rails_helper.rb'
-    run 'echo "require \'capybara/rspec\'" | cat - spec/spec_helper.rb > temp && mv temp spec/spec_helper.rb'
+    # Add Capybara configuration to rails_helper.rb after rspec/rails
+    rails_helper_path = 'spec/rails_helper.rb'
+    if File.exist?(rails_helper_path)
+      content = File.read(rails_helper_path)
+      
+      # Insert capybara/rails after rspec/rails
+      unless content.include?("require 'capybara/rails'")
+        content.gsub!(/require 'rspec\/rails'/, "require 'rspec/rails'\nrequire 'capybara/rails'")
+        File.write(rails_helper_path, content)
+        puts "✅ Added Capybara to rails_helper.rb"
+      end
+    end
+    
+    # Add capybara/rspec to spec_helper.rb at the top
+    spec_helper_path = 'spec/spec_helper.rb'
+    if File.exist?(spec_helper_path)
+      content = File.read(spec_helper_path)
+      
+      unless content.include?("require 'capybara/rspec'")
+        # Insert at the beginning after the comment
+        content.sub!(/^(# [^\n]+\n)/, "\\1require 'capybara/rspec'\n")
+        File.write(spec_helper_path, content)
+        puts "✅ Added Capybara to spec_helper.rb"
+      end
+    end
 
   end
 end
